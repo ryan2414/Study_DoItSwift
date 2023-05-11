37cc1fb842b5a428090d1aaa4bdff1d54b941e3b
ryan2414 <jackpar@nate.com>
2023-05-11 23:23:42 +0900

    Study Tap controller

---
+++ Tap/Tab/Tab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by 이승재 on 2023/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
