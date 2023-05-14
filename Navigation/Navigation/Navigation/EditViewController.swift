ec270b355ee4a49bb8eb14062a8f84841ff09116
ryan2414 <jackpar@nate.com>
2023-05-15 07:42:49 +0900

    Navigation

--- Navigation/Navigation/Navigation/EditViewController.swift
+++ Navigation/Navigation/Navigation/EditViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol EditDelegate {
     func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
 }
 
 class EditViewController: UIViewController {
@@ -16,9 +17,11 @@ class EditViewController: UIViewController {
     var textWayValue: String = ""
     var textMessage: String = ""
     var delegate : EditDelegate?
+    var isOn = false
     
     @IBOutlet var lblWay: UILabel!
     @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swisOn: UISwitch!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,13 +29,23 @@ class EditViewController: UIViewController {
         // Do any additional setup after loading the view.
         lblWay.text = textWayValue
         txMessage.text = textMessage
+        swisOn.isOn = false
     }
     
     @IBAction func btnDone(_ sender: UIButton) {
         if delegate != nil {
             delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
         }
         _ = navigationController?.popViewController(animated: true)
+    }
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn{
+            isOn = true
+        }
+        else{
+            isOn = false
+        }
     }
     
     /*
